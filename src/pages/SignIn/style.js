f70e30e39d9bfffb2a49a7d17876a77c5019d4ba
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-01-03 10:43:09 -0300

    feat: sign-in page layout

---
+++ src/pages/SignIn/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.div`
+  width: 535px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Link = styled.p`
+  font-weight: 400;
+  font-size: 20px;
+  color: #ffffff;
+  text-decoration: underline;
+`;
---
+++ src/styles/Logo.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Sidebar = styled.header`
+  width: calc(100% - 535px);
+  background: #151515;
+`;
+
+export const Header = styled.header`
+  font-family: "Passion One", sans-serif;
+  font-size: 106px;
+  letter-spacing: 5px;
+  color: white;
+  margin-top: 300px;
+    margin-left: 144px;
+`;
+
+export const Subtile = styled.h1`
+  max-width: 442px;
+  font-weight: 700;
+  font-size: 43px;
+  letter-spacing: 0;
+  margin-left: 144px;
+`;
