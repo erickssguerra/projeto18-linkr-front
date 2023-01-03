4518af6f0f4d9c50f8e36e872534c56f6d3d7662
Bruno Baroni <brunobaronims@gmail.com>
2023-01-03 18:17:48 -0300

    Add header and div structure

---
+++ src/components/Header/style.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1rem;
+  height: 5rem;
+  width: 100%;
+  background: #151515;
+`;
+
+export const HeaderText = styled.h1`
+  color: #FFF;
+  font-family: 'Passion One', sans-serif;
+  font-size: 4rem;
+`
+
+export const HeaderMenu = styled.div`
+
+`;
+
+export const MenuSelect = styled.button`
+
+`;
+
+export const MenuImage = styled.img`
+
+`;
---
+++ src/pages/Home/style.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  margin: 4rem 0 2rem 0;
+  display: flex;
+  width: 40rem;
+  justify-content: left;
+  color: #FFF;
+  font-family: 'Passion One', sans-serif;
+  font-size: 4rem;
+`;
+
+export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const MainContent = styled.section`
+  width: 40rem;
+`
+
+export const PublishDialog = styled.div`
+  margin-bottom: 2rem;
+  width: 40rem;
+`;
+
+export const Posts = styled.ul`
+  width: 40rem;
+`;
