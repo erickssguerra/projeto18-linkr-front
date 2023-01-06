d99974b67210702a0b0fc1262f4811a6edd7d17c
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-01-06 10:15:20 -0300

    feat: modal component

---
+++ src/components/Modal/style.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+  width: 21rem;
+  font-family: "Lato", sans-serif;
+  font-weight: 700;
+  font-size: 2rem;
+  color: #ffffff;
+  text-align: center;
+`;
+
+export const ButtonContainer = styled.div`
+  width: 19rem;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 2rem;
+`;
+
+export const Close = styled.button`
+  width: 8rem;
+  height: 2rem;
+  background: #ffffff;
+  border-radius: 0.3rem;
+  border: none;
+  font-family: "Lato", sans-serif;
+  font-weight: 700;
+  font-size: 1rem;
+  color: #1877f2;
+  cursor: pointer;
+`;
+
+export const Comfirm = styled.button`
+  width: 8rem;
+  height: 2rem;
+  background: #1877f2;
+  border-radius: 0.3rem;
+  border: none;
+  font-family: "Lato", sans-serif;
+  font-weight: 700;
+  font-size: 1rem;
+  color: #ffffff;
+  cursor: pointer;
+`;
