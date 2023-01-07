ae098557df456d71351fd028cb92b3d37aec76fc
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-01-07 10:02:29 -0300

    feat: user posts page

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,10 +1,68 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Header from "../../components/Header";
+import { api } from "../../services";
+import * as Style from "./style";
+import { useAuth } from "../../providers";
+import PostsList from "../../components/PostsList";
+import TrendingCard from "../../components/TrendingCard";
 
 export default function UserPage() {
+  const [postsData, setPostsData] = useState(undefined);
+  const [isLoading, setLoading] = useState(0);
+  const { userAuth, setUserAuth } = useAuth();
+  const [postDeleted, setPostDeleted] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (!userAuth) setLoading(1);
+
+    if (userAuth) {
+      async function fetchData() {
+        setLoading(1);
+
+        try {
+          const res = await api.get(`/user/${id}`, {
+            headers: { Authorization: `Bearer ${userAuth.token}` },
+          });
+
+          setPostsData(res.data);
+          setLoading(0);
+        } catch (e) {
+          setLoading(0);
+          return navigate("*");
+        }
+      }
+
+      fetchData();
+    }
+  }, [userAuth, navigate, setUserAuth, setLoading, postDeleted]);
+
+  if (!userAuth || !postsData) return;
+
   return (
-    <>
+    <Style.Container>
       <Header />
-      UserPage
-    </>
+      <Style.UserInfos>
+        <Style.UserImg src={postsData.user.picture_url} />
+        <Style.Title>{postsData.user.name}'s posts</Style.Title>
+      </Style.UserInfos>
+      <Style.PageContent>
+        <Style.MainContent>
+          <PostsList
+            data={postsData.formattedData}
+            isLoading={isLoading}
+            postDeleted={postDeleted}
+            setPostDeleted={setPostDeleted}
+          />
+        </Style.MainContent>
+        <Style.TrendingDiv>
+          <TrendingCard />
+        </Style.TrendingDiv>
+      </Style.PageContent>
+    </Style.Container>
   );
 }
---
+++ src/pages/User/style.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  display: flex;
+  justify-content: left;
+  color: #fff;
+  font-family: "Oswald", sans-serif;
+  font-size: 3rem;
+
+  @media (max-width: 1024px) {
+    width: 100%;
+    font-size: 33px;
+    padding-left: 1rem;
+    margin: 2rem 0;
+  }
+`;
+
+export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const MainContent = styled.section`
+  width: 40rem;
+
+  @media (max-width: 1024px) {
+    width: 100%;
+  }
+`;
+
+export const PageContent = styled.div`
+  display: flex;
+
+  @media (max-width: 1024px) {
+    width: 100%;
+  }
+`;
+
+export const PublishAndPostsDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (max-width: 1024px) {
+    width: 100%;
+  }
+`;
+
+export const TrendingDiv = styled.div``;
+
+export const UserInfos = styled.div`
+width: 941px;
+display: flex;
+align-items: center;
+margin: 4rem 0 2rem 0;
+`;
+
+export const UserImg = styled.img`
+  height: 50px;
+  width: 50px;
+  border-radius: 2rem;
+  background: #fff;
+  margin: 0 1rem;
+
+  @media (max-width: 768px) {
+    height: 40px;
+    width: 40px;
+  }
+`;
