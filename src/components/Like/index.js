34bfec946a15bb246deb4589c96214da7dd8e370
erickssguerra <erickssguerra@gmail.com>
2023-01-09 14:15:41 -0300

    feat: likes description

--- src/components/Like/index.js
+++ src/components/Like/index.js
@@ -7,7 +7,7 @@ import { api, config } from "../../services";
 export default function Like({ likes, postId, updateData }) {
   const { userAuth } = useAuth();
   const [iconLike, setIconLike] = useState(false);
-
+  const [likesMessage, setLikesMessage] = useState("None liked yet");
   function postLike() {
     api
       .post(`/like/${postId}`, {}, config(userAuth.token))
@@ -21,19 +21,73 @@ export default function Like({ likes, postId, updateData }) {
   }
 
   useEffect(() => {
-    likes.forEach((like) => {
-      if (like.user_id === userAuth.userId) {
+    const arrayUsersId = [];
+    for (let i = 0; i < likes.length; i++) {
+      if (likes[i].user_id === userAuth.userId) {
         setIconLike(true);
       }
-    });
-  }, []);
+      arrayUsersId.push(Number(likes[i].user_id));
+    }
+    if (arrayUsersId.length == 1) {
+      setLikesMessage(`${likes[0].user_name?.split(" ")[0]}`);
+    }
+
+    if (arrayUsersId.includes(userAuth.userId) && arrayUsersId.length == 1) {
+      setLikesMessage("You");
+    }
+
+    if (arrayUsersId.length == 2) {
+      setLikesMessage(
+        `${likes[0].user_name?.split(" ")[0]} and ${
+          likes[1].user_name?.split(" ")[0]
+        }`
+      );
+    }
+
+    if (arrayUsersId.includes(userAuth.userId) && arrayUsersId.length == 2) {
+      const arrayUsersName = [];
+      for (let i = 0; i < 2; i++) {
+        if (userAuth.userId !== likes[i].user_id) {
+          arrayUsersName.push(likes[i].user_name?.split(" ")[0]);
+        }
+      }
+      setLikesMessage(`You and ${arrayUsersName[0]}`);
+    }
+
+    if (arrayUsersId.length > 2) {
+      const arrayUsersName = [];
+      for (let i = 0; i < 2; i++) {
+        arrayUsersName.push(likes[i].user_name?.split(" ")[0]);
+      }
+      setLikesMessage(
+        `${arrayUsersName[0]}, ${arrayUsersName[1]} and ${
+          arrayUsersId.length - 2
+        }`
+      );
+    }
+
+    if (arrayUsersId.length > 2 && arrayUsersId.includes(userAuth.userId)) {
+      const arrayUsersName = [];
+      for (let i = 0; i < arrayUsersId.length; i++) {
+        if (userAuth.userId !== likes[i].user_id) {
+          arrayUsersName.push(likes[i].user_name?.split(" ")[0]);
+          break;
+        }
+      }
+      setLikesMessage(
+        `You, ${arrayUsersName[0]} and ${arrayUsersId.length - 2}`
+      );
+    }
+  }, [updateData]);
 
   return (
     <>
       <S.Icon onClick={postLike}>
         {iconLike ? <Icons.FillHeart /> : <Icons.OutlineHeart />}
+        <S.LikesCount role="tooltip" aria-label={likesMessage}>
+          {likes.length} likes
+        </S.LikesCount>
       </S.Icon>
-      <S.LikesCount>{likes.length} likes</S.LikesCount>
     </>
   );
 }
--- src/components/Like/style.js
+++ src/components/Like/style.js
@@ -3,8 +3,57 @@ import styled from "styled-components";
 export const Icon = styled.div`
   font-size: 1rem;
   cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const LikesCount = styled.p`
-  font-size: .8rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 0.8rem;
+  margin-top: 2px;
+  text-align: center;
+  position: relative;
+  &:hover::after,
+  &:hover::before {
+    transition: 0.2s;
+    opacity: 1;
+    pointer-events: all;
+  }
+
+  &::after {
+    top: 25px;
+    white-space: nowrap;
+    pointer-events: none;
+    transition: 0.2s;
+    content: attr(aria-label);
+    color: #505050;
+    background: white;
+    border-radius: 3px;
+    padding: 5px 10px;
+    opacity: 0;
+    position: absolute;
+    z-index: 2;
+    overflow: hidden;
+  }
+
+  &::before {
+    top: 20px;
+    pointer-events: none;
+    content: "";
+    color: white;
+    position: absolute;
+    width: 13px;
+    height: 13px;
+    background: white;
+    opacity: 0;
+    transform: rotate(45deg);
+    z-index: 1;
+    background: white;
+    transition: 0.1s;
+    overflow: hidden;
+  }
 `;
