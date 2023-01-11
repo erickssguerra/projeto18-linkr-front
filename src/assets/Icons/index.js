44581242c5f50c884507f4e62fd6cc716327d0ce
erickssguerra <erickssguerra@gmail.com>
2023-01-11 01:23:51 -0300

    feat: repost layout (inside)

--- src/assets/Icons/index.js
+++ src/assets/Icons/index.js
@@ -1,9 +1,14 @@
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { FaRetweet } from "react-icons/fa";
 
 export function OutlineHeart() {
   return <AiOutlineHeart />;
 }
 
-export function FillHeart(){
-    return <AiFillHeart color="red" />
+export function FillHeart() {
+  return <AiFillHeart color="red" />;
+}
+
+export function Repost() {
+  return <FaRetweet size={20} />;
 }
