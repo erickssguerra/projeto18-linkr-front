45ba10a11288673d7288457f0555f16f53b0a125
erickssguerra <erickssguerra@gmail.com>
2023-01-13 17:12:22 -0300

    fix: hide console.logs in RepostBar

--- src/components/RepostBar/index.js
+++ src/components/RepostBar/index.js
@@ -19,10 +19,10 @@ export default function RepostBar({ authorId, postId }) {
       })
       .catch((err) => console.log(err.response.data));
   }, []);
-console.log(`isFollower? ${isFollower}`)
-console.log(`authorId = ${authorId}`)
-console.log(`postId = ${postId}`)
-console.log(`userId = ${userAuth.userId}`)
+// console.log(`isFollower? ${isFollower}`)
+// console.log(`authorId = ${authorId}`)
+// console.log(`postId = ${postId}`)
+// console.log(`userId = ${userAuth.userId}`)
   return (
     <S.Container isFollower={isFollower}>
       <Icons.Repost /> Re-posted by you
